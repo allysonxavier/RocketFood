5666b2ab1b84be8126ae7edf0b5fa550aa967317
Allyson Xavier <allysonxavier3@gmail.com>
2024-03-03 16:13:23 -0300

    feat: banner home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import { Footer } from "../../components/Footer";
 import {BagComponent} from "../../components/BagComponent/index.jsx";
 import * as S from './styles.js';
 import MaskGroup from '../../assets/Maskgroup.png';
+import Doces from '../../assets/docescaindo.png'
+import {Wrapper} from "./styles.js";
 
 const itemExample = {
     name: "Camisa Casual Azul",
@@ -17,14 +19,21 @@ const itemExample2 = {
 
 export function Home() {
     return (
-        <div>
+        <S.Wrapper>
             <Header />
+            <S.BannerBox>
+                <img src={Doces} alt="Doces"/>
+                <div>
+                    <h2>Sabores inigualáveis</h2>
+                    <p>Sinta o cuidado do preparo com ingredientes selecionados.
+                    </p>
+                </div>
+            </S.BannerBox>
+            <S.Container>
             <h1>Refeições</h1>
-            <S.Container >
             <BagComponent itemName={itemExample.name} itemPrice={itemExample.price} itemImageUrl={MaskGroup} />
             </S.Container>
             <Footer />
-
-        </div>
+        </S.Wrapper>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,21 +1,56 @@
 import styled from "styled-components";
 
+export  const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    max-width: 428px;
+    margin: 0 auto;
+    `
 export const Container = styled.div`
     display: flex;  
     width: 100%;
-    max-width: 300px;
-    //padding: 24px;
     height: 100%;
-    align-items: center;
-    //gap: 16px;
     flex-direction: column;
->div{
-        display: flex;
-        align-items: center;
-        text-align: center ;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        //gap: 14px;
+    padding: 24px;
+    h1 {
+        text-align: left;
+        margin-bottom: 24px;        
     }
-    `;
+`;
+
+export const BannerBox = styled.div`
+    width: 100%;
+    height: 120px;
+    max-width: 376px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-image: linear-gradient(to right, #091E26, #00131C);
+    margin: 44px auto 62px;
+
+     img {
+         width: 191px;
+         height: 149px;
+        margin-left: -32px;
+        margin-top: -24px;
+    }
+    
+    > div {
+        width: 100%;
+        h2 {
+            font-family: "Poppins", sans-serif;
+            font-size: 16px;
+            font-weight: 600;
+            margin-bottom: 3px;
+        }
+
+        p {
+            font-family: "Poppins", sans-serif;
+            font-size: 12px;
+            text-align: left;
+        }
+    }
+`
